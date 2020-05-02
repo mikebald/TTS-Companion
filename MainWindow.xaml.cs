57a0aa267b2b4259c8354d39f0a707c9aa1de8d5
mikebald <mikebald@gmail.com>
2020-05-02 15:39:32 -0400

    Google API Support
    Expanded TTS to use Google's Text to Speech cloud engine

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -11,6 +11,8 @@ using System.Speech.Synthesis;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using Google.Cloud.TextToSpeech.V1;
+
 
 namespace TTS_Companion
 {
@@ -21,61 +23,116 @@ namespace TTS_Companion
     {
         private readonly SpeechSynthesizer _speechSynthesizer = new SpeechSynthesizer();
         private LowLevelKeyboardListener _listener;
+        private bool _currentKeyPress;
+
+        TextToSpeechClient _client;
+        VoiceSelectionParams _voiceParms;
+        AudioConfig _audioConfig;
 
         public MainWindow()
         {
             InitializeComponent();
+            InitializeGoogleEnvironmentVariable();
             InitializeVoice();
             InitializeKeyboardListener();
         }
 
+        private void InitializeGoogleEnvironmentVariable()
+        {
+            System.Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", "C:\\Users\\mikeb\\google_TTS_key.json");
+        }
+
         private void InitializeKeyboardListener()
         {
+            _currentKeyPress = false;
             _listener = new LowLevelKeyboardListener();
             _listener.OnKeyPressed += GlobalKeyboard_OnKeyPressed;
-
             _listener.HookKeyboard();
         }
 
         private void InitializeVoice()
         {
-            _speechSynthesizer.SelectVoiceByHints(VoiceGender.Male);
-            _speechSynthesizer.Rate = 1;
-            _speechSynthesizer.Volume = 60;
+            _client = TextToSpeechClient.Create();
+
+            // Build the voice request.
+            _voiceParms = new VoiceSelectionParams
+            {
+                LanguageCode = "en-US",
+                SsmlGender = SsmlVoiceGender.Male
+            };
+
+            // Specify the type of audio file.
+            _audioConfig = new AudioConfig
+            {
+                AudioEncoding = Google.Cloud.TextToSpeech.V1.AudioEncoding.Mp3,
+                Pitch = 0.0
+            };
+
+        }
+
+        private void GoogleSay(string message)
+        {
+            using (var stream = new MemoryStream())
+            {
+                
+                // The input to be synthesized, can be provided as text or SSML.
+                var input = new SynthesisInput
+                {
+                    Text = message
+                };
+
+                // Perform the text-to-speech request.
+                var response = _client.SynthesizeSpeech(input, _voiceParms, _audioConfig);
+
+                response.AudioContent.WriteTo(stream);
+                
+                using (var waveOut = new WaveOut { Device = new WaveOutDevice(2) })
+                using (var waveSource = new MediaFoundationDecoder(stream))
+                {
+                    waveOut.Initialize(waveSource);
+                    waveOut.Play();
+                    waveOut.WaitForStopped();
+                }
+            }
         }
 
         private void GlobalKeyboard_OnKeyPressed(object sender, KeyPressedArgs e)
         {
-            if(e.KeyPressed == System.Windows.Input.Key.Up)
+            if (!_currentKeyPress)
             {
-                Speak("Yes");
-            }
+                _currentKeyPress = true;
+                if (e.KeyPressed == System.Windows.Input.Key.Up)
+                {
+                    GoogleSay("Yes");
+                }
 
-            if (e.KeyPressed == System.Windows.Input.Key.Down)
-            {
-                Speak("No");
-            }
+                if (e.KeyPressed == System.Windows.Input.Key.Down)
+                {
+                    GoogleSay("No");
+                }
 
-            if (e.KeyPressed == System.Windows.Input.Key.Left)
-            {
-                Speak("Being Ganked");
-            }
+                if (e.KeyPressed == System.Windows.Input.Key.Left)
+                {
+                    GoogleSay("I am Being Ganked");
+                }
 
-            if (e.KeyPressed == System.Windows.Input.Key.Right)
-            {
-                Speak("Last district");
-            }
+                if (e.KeyPressed == System.Windows.Input.Key.Right)
+                {
+                    GoogleSay("Last district");
+                }
 
-            /*
-            if(e.KeyPressed == System.Windows.Input.Key.Oem5) // Backslash
-            {
-                Keyboard.Focus(txtSpeech);
-            }*/
+                /*
+                if(e.KeyPressed == System.Windows.Input.Key.Oem5) // Backslash
+                {
+                    Keyboard.Focus(txtSpeech);
+                }*/
+                _currentKeyPress = false;
+            }
         }
 
         private void SpeakString_OnClick(object sender, RoutedEventArgs e)
         {
-            Speak(txtSpeech.Text);
+            GoogleSay(txtSpeech.Text);
         }
 
         private void txtSpeech_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
@@ -87,26 +144,9 @@ namespace TTS_Companion
             }
         }
 
-        private void Speak(string message)
-        {
-            using (var stream = new MemoryStream())
-            {
-                _speechSynthesizer.SetOutputToWaveStream(stream);
-                _speechSynthesizer.Speak(message);
-
-                using (var waveOut = new WaveOut { Device = new WaveOutDevice(2) })
-                using (var waveSource = new MediaFoundationDecoder(stream))
-                {
-                    waveOut.Initialize(waveSource);
-                    waveOut.Play();
-                    waveOut.WaitForStopped();
-                }
-            }
-        }
-
         private void Speak_Content_OnClick(object sender, RoutedEventArgs e)
         {
-            Speak(((Button)sender).Content.ToString());
+            GoogleSay(((Button)sender).Content.ToString());
         }
 
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
